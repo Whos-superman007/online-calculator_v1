b969d6025df56e29c54f0628faa1ba73d2dd21c5
JonasReitmayr <jonas.reitmayr@outlook.de>
2025-06-12 18:14:51 +0200

    move to diffrent location

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Taschenrechner</title>
-    <link rel="stylesheet" href="../css/style-calculator.css">
+    <link rel="stylesheet" href="style-calculator.css">
 </head>
 <body>
     <div class="calculator">
